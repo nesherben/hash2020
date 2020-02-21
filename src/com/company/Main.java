c38e7245c450855df4d632402f49aa7ec3f4b75d
nesherben <nestor8299@gmail.com>
2020-02-21 09:38:30 +0100

    rutas en local generales

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -11,7 +11,7 @@ public class Main {
         ArrayList<Libreria> librerias = new ArrayList<>();
         ArrayList<Integer> scores = new ArrayList<>();
         /********ENTRADA DE DATOS********/
-        Scanner sc = new Scanner(new File("D:/Desktop/hash2020/f_libraries_of_the_world.txt"));
+        Scanner sc = new Scanner(new File("./inputs/a_example.txt"));
         //while (sc.hasNextLine()){
             b = sc.nextInt(); //Primera línea
             l = sc.nextInt();
